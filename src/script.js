f75997af59172e5e03aac149312cb11986b26145
Saim <saimhurhasan@cedcoss.com>
2022-02-08 12:03:49 +0530

    correction commit

--- src/script.js
+++ src/script.js
@@ -1,19 +1,23 @@
 parsedata=[];
+
 function start(){
-    oobj=fetch();
-    disp(oobj);
+    console.log("PORED");
+    fetch();
+    disp(parsedata);
 
 }
 function fetch() {
     let proid= document.getElementById("productid").value;
     let proname= document.getElementById("productname").value;
     let proprice=document.getElementById("productprice").value;
+
     obj={"id":proid,"name":proname,"price":proprice};
     console.log(obj);
-    parsedata.push();
-    return obj;
-
-    }
+    parsedata.push(obj);
+    console.log("update");
+    console.log(parsedata);
+      }
+    /*
 function disp(entry){
     let table=document.getElementById("olisttable");
     tr=document.createElement("tr");
@@ -22,4 +26,16 @@ function disp(entry){
     <td> ${entry["price"]}</td>`
     tr.innerHTML=ndata;
     table.appendChild(tr);
+}*/
+
+function disp(parsedata) {
+let html=" ";
+document.getElementById("olisttable").innerHTML= '<tr><th class="parse_right">Product ID</th><th> Product Name </th><th>Product Price</th></tr>';
+
+for(var i=0;i<parsedata.length;i++) {+
+    console.log(i);
+    let add='<tr><td>'+ parsedata[i].id+'</td><td>'+parsedata[i].name+'</td><td>'+parsedata[i].price+'</td>  </tr>';
+
+document.getElementById("olisttable").innerHTML+=add;
+}
 }
